ae078e310f64e53316613c83af1d6c2f6163c502
Marcos Rocha <mmurilo@gmail.com>
2023-02-18 08:16:38 +0000

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM python:3.11 as build
+
+COPY requirements ./
+
+RUN pip install --user --no-cache-dir -r requirements
+
+RUN python -m unittest discover
+
+# app stage
+FROM tiangolo/uvicorn-gunicorn-fastapi:python3.11-slim as app
+
+COPY --from=build /root/.local /root/.local
+
+RUN apt-get update && \
+    apt-get -y install sqlite3 && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
+ENV PATH=/root/.local/bin:$PATH
+
+COPY main.py .
+
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "80"]
---
+++ main.py
@@ -0,0 +1,86 @@
+import os
+import random
+import sqlite3
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from datetime import datetime
+from cachetools import cached, TTLCache
+
+
+app = FastAPI()
+
+
+class Word(BaseModel):
+    word: str
+
+
+db_path = os.environ.get('DB_PATH', 'api_calls.db')
+max_calls = int(os.environ.get('MAX_CALLS', 100))
+return_limit = int(os.environ.get('RETURN_LIMIT', 10))
+
+with sqlite3.connect(db_path) as conn:
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS api_calls (
+            id INTEGER PRIMARY KEY,
+            timestamp TEXT,
+            input_word TEXT,
+            random_word TEXT
+        )
+    ''')
+    conn.commit()
+    cursor.execute(f'''
+        DELETE FROM api_calls WHERE id NOT IN (
+            SELECT id FROM api_calls ORDER BY timestamp DESC LIMIT {max_calls}
+        )
+    ''')
+    conn.commit()
+
+
+def add_api_call(input_word, random_word):
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    with sqlite3.connect(db_path) as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+            INSERT INTO api_calls (timestamp, input_word, random_word)
+            VALUES (?, ?, ?)
+        ''', (timestamp, input_word, random_word))
+        conn.commit()
+
+
+# caches the results of get_api_calls for 10 seconds
+@cached(cache=TTLCache(maxsize=128, ttl=10))
+def get_api_calls(limit=return_limit):
+    with sqlite3.connect(db_path) as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+            SELECT timestamp, input_word, random_word
+            FROM api_calls
+            ORDER BY timestamp DESC
+            LIMIT ?
+        ''', (limit,))
+        rows = cursor.fetchall()
+        api_calls = []
+        for row in rows:
+            api_calls.append({
+                "timestamp": row[0],
+                "input_word": row[1],
+                "random_word": row[2]
+            })
+        return {"api_calls": api_calls}
+
+
+@app.post("/jumble")
+def randomize_word(word: Word):
+    input_word = word.word
+    word_list = list(input_word)
+    random.shuffle(word_list)
+    random_word = ''.join(word_list)
+    add_api_call(input_word, random_word)
+    return {"random_word": random_word}
+
+
+@app.get("/audit")
+def api_calls(request: Request):
+    limit = request.query_params.get('limit', return_limit)
+    return get_api_calls(limit=limit)
